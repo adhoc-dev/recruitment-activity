a2eb3e345cbac55c63be7a2d5a5c9b92eb4ddcd2
Lara <lav@adhoc.inc>
2026-03-16 16:30:33 -0300

    [IMP] nueva versión ejercicio

--- real_state_management/models/estate_property.py
+++ real_state_management/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -25,15 +25,15 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many("estate.property.tag", string="Etiquetas")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Ofertas", required=True)
     total_area = fields.Float(compute="_compute_total_area")
-    best_price = fields.Float(compute="_compute_best_price")
+    # best_price = fields.Float(compute="_compute_best_price")
 
     @api.depends('garden_area', 'living_area')
     def _compute_total_area(self):
         for rec in self:
             rec.total_area = 0
 
-    @api.depends('offer_ids.price')
-    def _compute_best_price(self):
-        for rec in self:
-            rec.best_price = max(rec.offer_ids.mapped('price') or [0])
+    # @api.depends('offer_ids.price')
+    # def _compute_best_price(self):
+    #     for rec in self:
+    #         rec.best_price = max(rec.offer_ids.mapped('price') or [0])
 
